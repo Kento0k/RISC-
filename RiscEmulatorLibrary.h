4ec66e73b4b0370bbe09aea23ecf1a1034db2980
Kento_0k <asassin661@mail.ru>
2018-11-22 16:38:48 +0300

    new

--- RiscEmulatorLibrary.h
+++ RiscEmulatorLibrary.h
@@ -10,16 +10,13 @@
 using  namespace std;
 //instruction structure
 class RISC {
-public:
+private:
     struct instruction{
         vector<int> args;
         int address;
         string name;
     };
     typedef struct instruction instruction;
-
-//Еrror processing functions
-    void error_processing(int code);
 
 //File parsing functions
     void parse_file(string& text, vector<instruction>& program);
@@ -28,14 +25,17 @@ public:
 
 //Writing program to memory
     void memory_write(vector<vector<int>>& memory, RISC::instruction step);
-
+public:
 //program execution
     void exec_program(string& inpName, string& outName, vector<vector<int>>& reg,  vector<vector<int>>& memory);
+
+    //operations
+    void run_instruction(vector<vector<int>>& reg, vector<vector<int>>& memory, int *PC, int *maxAcessAdress);
 
 //program_debugging
     void debug_program(string& inpName, string& outName, vector<vector<int>>& reg,  vector<vector<int>>& memory);
 
-//operations
-    void run_instruction(vector<vector<int>>& reg, vector<vector<int>>& memory, int *PC, int *maxAcessAdress);
+    //Еrror processing functions
+    void error_processing(int code);
 };
 #endif //RISC_RISCEMULATORLIBRARY_H
